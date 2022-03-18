e7f6dff2524789ebe25ee7a92bea48450175d9a9
wangshan <22212@qq.com>
2022-03-19 04:05:55 +0800

    feat: 事件相关（feature）

--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-03-19 03:41:07
+ * @LastEditTime: 2022-03-19 03:59:21
  * @Description: 开关组件
  * @Date: 2022-03-19 03:27:39
  * @Author: wangshan
@@ -14,10 +14,14 @@ export default class Toggle extends React.Component {
     };
 
     // 绑定this
-    this.handleSwitch = this.handleSwitch.bind(this);
+    this.handleSwitchP = this.handleSwitchP.bind(this);
   }
 
-  handleSwitch(e) {
+  // 为button注册handler
+
+  // 解决this丢失
+  // bind
+  handleSwitchP(e) {
     console.log(e);
     this.setState((state) => {
       return {
@@ -25,6 +29,20 @@ export default class Toggle extends React.Component {
       };
     });
   }
+
+  // public class fields
+  handleSwitch = (e) => {
+    console.log(e);
+    this.setState((state) => {
+      return {
+        isOff: !state.isOff,
+      };
+    });
+  };
+
+  // 或者为事件绑定箭头函数
+  // 注意如果将回调函数作为prop往下传递，在每次组件渲染时，会创建不同回调函数，导致性能问题.
+
   render() {
     return (
       <div className="toggle-wrap">
--- src/views/Event/index.jsx
+++ src/views/Event/index.jsx
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-03-19 03:33:32
+ * @LastEditTime: 2022-03-19 04:03:59
  * @Description: 事件处理
  * @Date: 2022-03-19 02:01:20
  * @Author: wangshan
@@ -52,6 +52,23 @@ export default function EventHandle() {
           <p>注册Toggle</p>
           <Toggle />
         </div>
+      </div>
+      <div>
+        <h3 className="tag">向事件处理程序传递参数</h3>
+        <p>
+          在循环中，通常我们会为事件处理函数传递额外的参数。例如，若 id
+          是你要删除那一行的 ID，以下两种方式都可以向事件处理函数传递参数：
+        </p>
+        <code className="tag">
+          {`<button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button>
+<button onClick={this.deleteRow.bind(this, id)}>Delete Row</button>`}
+        </code>
+        <p>
+          上述两种方式是等价的，分别通过箭头函数和 Function.prototype.bind
+          来实现。在这两种情况下，React 的事件对象 e
+          会被作为第二个参数传递。如果通过箭头函数的方式，事件对象必须显式的进行传递，而通过
+          bind 的方式，事件对象以及更多的参数将会被隐式的进行传递。
+        </p>
       </div>
     </div>
   );
