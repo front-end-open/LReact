1f19eb04a739171781f9c6f22d9b42bea8bacddc
wangshan <22212@qq.com>
2022-03-19 02:58:54 +0800

    feat: 添加公共缺失页面（feature）

--- src/views/common/404.jsx
+++ src/views/common/404.jsx
@@ -1,10 +1,30 @@
 /*
- * @LastEditTime: 2022-03-19 00:18:24
+ * @LastEditTime: 2022-03-19 02:58:29
  * @Description:
  * @Date: 2022-03-17 00:35:02
  * @Author: wangshan
  * @LastEditors: wangshan
  */
+import "./common.scss";
 export default function () {
-  return <h1>no thing found</h1>;
+  return (
+    <div className="container">
+      <div className="noise"></div>
+      <div className="overlay"></div>
+      <div className="terminal">
+        <h1>
+          Error <span className="errorcode">404</span>
+        </h1>
+        <p className="output">
+          The page you are looking for might have been removed, had its name
+          changed or is temporarily unavailable.
+        </p>
+        <p className="output">
+          Please try to <a href="#1">go back</a> or{" "}
+          <a href="#2">return to the homepage</a>.
+        </p>
+        <p className="output">Good luck.</p>
+      </div>
+    </div>
+  );
 }
