df90ae6676505078768bab4d2dc445098765c9c7
wangshan <22212@qq.com>
2022-03-23 00:36:31 +0800

    feat: modal测试（feature）

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,43 +1,25 @@
 /*
- * @LastEditTime: 2022-03-23 00:19:44
+ * @LastEditTime: 2022-03-23 00:32:47
  * @Description: modal对话框
  * @Date: 2022-03-23 00:07:56
  * @Author: wangshan
  * @LastEditors: wangshan
  */
 import "./index.scss";
-export default class Modal extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      visible: false,
-    };
-  }
-
-  handleOpen = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  handleClose = () => {
-    this.setState({
-      visible: false,
-    });
-  };
-
-  render() {
-    return (
-      <div className="window">
-        <div className="modal">
-          <div className="modal-title">{this.props.title}</div>
-          <div className="modal-content">{this.props.children}</div>
-          <div className="modal-footer">
-            <button onClick={this.handleClose}>取消</button>
-            <button onClick={this.handleClose}>确认</button>
-          </div>
+export default function Modal(props) {
+  let visible = props.visible;
+  return visible ? (
+    <div className="window">
+      <div className="modal">
+        <div className="modal-title">{props.title}</div>
+        <div className="modal-content">{props.children}</div>
+        <div className="modal-footer">
+          <button onClick={props.onClick}>取消</button>
+          <button onClick={props.onClick}>确认</button>
         </div>
       </div>
-    );
-  }
+    </div>
+  ) : (
+    ""
+  );
 }
--- src/views/combination/index.jsx
+++ src/views/combination/index.jsx
@@ -1,13 +1,32 @@
-import FancyBorder from "./components/FancyBorder";
-
 /*
- * @LastEditTime: 2022-03-23 00:00:03
+ * @LastEditTime: 2022-03-23 00:34:50
  * @Description: 组合
  * @Date: 2022-03-22 23:40:00
  * @Author: wangshan
  * @LastEditors: wangshan
  */
+import Modal from "@/components/Modal";
+import FancyBorder from "./components/FancyBorder";
 export default class Combination extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      visible: false,
+    };
+  }
+
+  handleOpen = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
   render() {
     return (
       <div>
@@ -53,6 +72,31 @@ export default class Combination extends React.Component {
                 <FancyBorder color="pink">
                   <p>Register Fanycyborder</p>
                 </FancyBorder>
+              </div>
+            </div>
+
+            <div>
+              <h3 className="tip-succeess">Modal</h3>
+              <div>
+                <Modal
+                  title="welcome Pachverb"
+                  visible={this.state.visible}
+                  onClick={this.handleClose}
+                >
+                  <h3>那么继承呢？</h3>
+                  <p>
+                    在 Facebook，我们在成百上千个组件中使用
+                    React。我们并没有发现需要使用继承来构建组件层次的情况。
+                    Props
+                    和组合为你提供了清晰而安全地定制组件外观和行为的灵活方式。注意：组件可以接受任意
+                    props，包括基本数据类型，React 元素以及函数。
+                    如果你想要在组件间复用非 UI
+                    的功能，我们建议将其提取为一个单独的 JavaScript
+                    模块，如函数、对象或者类。组件可以直接引入（import）而无需通过
+                    extend 继承它们。
+                  </p>
+                </Modal>
+                <button onClick={this.handleOpen}>open</button>
               </div>
             </div>
           </div>
