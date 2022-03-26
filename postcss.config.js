5a2a3a257b17d19b4409ae3a0b99c096652f0cc3
wangshan <22212@qq.com>
2022-03-27 02:13:38 +0800

    chore: 代码个格式化

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 /*
- * @LastEditTime: 2022-03-27 01:51:23
+ * @LastEditTime: 2022-03-27 02:13:26
  * @Description: css标识符
  * @Date: 2022-03-15 01:11:45
  * @Author: wangshan
  * @LastEditors: wangshan
  */
 module.exports = {
-  plugins: [require("autoprefixer")],
-};
+    plugins: [require('autoprefixer')]
+}
