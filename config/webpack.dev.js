02cf424e0aa7c4167796af0be1b4938d17014eb4
wangshan <22212@qq.com>
2022-03-26 23:32:52 +0800

    perf: webapck配置单个文件性能提示阈值
    - 入口资源文件最大400kb默认
    - 出口资源文件100kb
    - 对资源文件的限制均做警告提示

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-03-26 23:12:43
+ * @LastEditTime: 2022-03-26 23:29:52
  * @Description: 开发环境配置
  * @Date: 2022-03-15 00:33:30
  * @Author: wangshan
@@ -10,6 +10,11 @@ const BundleAnalyzerPlugin =
 module.exports = require("./webpack.common")({
   mode: "development",
   devtool: "inline-source-map",
+  performance: {
+    // 开启性能提示
+    hints: "warning",
+    maxAssetSize: 100000, // 100kb 输出单个资源文件的体积阈值提示
+  },
   plugins: [
     new BundleAnalyzerPlugin({
       analyzerMode: "disabled", // 不启动展示打包报告的http服务器
