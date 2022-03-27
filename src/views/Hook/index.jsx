70f1400b2a61a30e08cb2bc1147d5b67d23ce229
wangshan <22212@qq.com>
2022-03-28 01:20:13 +0800

    feat: reduxhook使用（feature）

---
+++ src/views/Hook/index.jsx
@@ -0,0 +1,14 @@
+/*
+ * @LastEditTime: 2022-03-28 01:19:22
+ * @Description: React-hook
+ * @Date: 2022-03-28 01:19:21
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+export default function Hooks() {
+    return (
+        <div>
+            <h3 className="tip-success">hook使用</h3>
+        </div>
+    )
+}
