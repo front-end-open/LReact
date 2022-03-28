66de6c89b0b05dc7816e3d14d8859119e65f5484
wangshan <22212@qq.com>
2022-03-29 00:00:40 +0800

    feat: useState使用

---
+++ src/views/Hook/components/useStateUse.jsx
@@ -0,0 +1,36 @@
+/*
+ * @LastEditTime: 2022-03-28 23:46:14
+ * @Description: useState使用示列
+ * @Date: 2022-03-28 22:21:50
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+// useState
+
+// console.log(a)
+export default function UseState(props) {
+    //经测试声明在函数组价外部的Hook无效。组件不会渲染
+    // 使用useState声明count
+    const [count, setCount] = React.useState(0)
+    // 声明多个 state 变量！
+    const [age, setAge] = React.useState(42)
+    const [fruit, setFruit] = React.useState('banana')
+    const [todos, setTodos] = React.useState([{ text: 'Learn Hooks' }])
+    return (
+        <div>
+            <h3>count: {count}</h3>
+            <div>
+                <span>点击更新count:</span>
+                <button onClick={() => setCount(count + 1)}>Click me</button>
+            </div>
+            <div>
+                <span>声明多state</span>
+                <ul>
+                    <li>age: {age}</li>
+                    <li>fruit: {fruit}</li>
+                    <li>todos: {todos[0].text}</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
--- src/views/Hook/index.jsx
+++ src/views/Hook/index.jsx
@@ -1,14 +1,28 @@
 /*
- * @LastEditTime: 2022-03-28 01:19:22
+ * @LastEditTime: 2022-03-28 23:39:36
  * @Description: React-hook
  * @Date: 2022-03-28 01:19:21
  * @Author: wangshan
  * @LastEditors: wangshan
  */
+
+import UseStates from './components/useStateUse'
+
 export default function Hooks() {
     return (
         <div>
             <h3 className="tip-success">hook使用</h3>
+            <div>
+                <h3 className="tip-success">Hook初次使用：useState</h3>
+                <div>
+                    <ul className="primary">
+                        <h3 className="tip-error">useState返回值说明:</h3>
+                        <li>返回一个state</li>
+                        <li>返回更新state的dispathAction-func</li>
+                    </ul>
+                    <UseStates />
+                </div>
+            </div>
         </div>
     )
 }
