d9c701f739c06467c5770c94402c2b24effe8288
wangshan <22212@qq.com>
2022-04-08 18:00:55 +0800

    feat: 手动不可变性

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-04-08 16:47:17
+ * @LastEditTime: 2022-04-08 16:54:45
  * @Description: 应用状态store
  * @Date: 2022-03-28 00:51:24
  * @Author: wangshan
@@ -9,15 +9,20 @@ import { createStore } from 'redux'
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
 import counterReducer, { Counter } from '@/reducers/counterReducer'
+console.log(counterReducer)
 /**
  * redux状态机：默认包含一个根级别的reducer
  * 然后根级别的rootReducer在组合其他的切片子级reducer
  *
  */
 export default configureStore({
-    reducer: {
+    // reducer: {
+    //     // 传递切片对象，默认redux会将切片reducers传递给combineReducers.
+    //     counter: counterReducer
+    // }
+    reducer: combineReducers({
         counter: counterReducer
-    }
+    })
 })
 
 export const BaseCounter = createStore(Counter)
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-04-08 16:30:18
+ * @LastEditTime: 2022-04-08 18:00:46
  * @Description: redux切片
  * @Date: 2022-03-28 01:05:24
  * @Author: wangshan
@@ -8,31 +8,49 @@
 // Reduxjs/tookit 版本
 // 切片”是应用中单个功能的 Redux reducer 逻辑和 action 的集合, 用来切分根状态对象
 import { createSlice } from '@reduxjs/toolkit'
-
+//  createSlice 的函数，它负责生成 action 类型字符串、action creator 函数和 action 对象的工作
 export const counterSlice = createSlice({
-    name: 'counters', // 命名此state;同时也用于标记reduce
+    name: 'counters', // name用作action的type
     initialState: {
         value: 0
     },
     reducers: {
         increment: (state) => {
             // type: counters/increment
-            state.value += 1
+            return {
+                ...state,
+                value: state.value + 1
+            }
+            // state.value += 1 // error: 不允许直接修改state对象
         },
         decrement: (state) => {
             // type: counters/decrement
-            state.value -= 1
+
+            return {
+                ...state,
+                value: state.value - 1
+            }
+            // state.value -= 1 // error: 不允许直接修改state对象
         },
         incrementByAmount: (state, action) => {
             // type: counters/incrementByAmount
-            state.value += action.payload
+            return {
+                ...state,
+                value: state.value + action.payload
+            }
+            // state.value += action.payload  // error: 不允许直接修改state对象
         }
     }
 })
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment, decrement, incrementByAmount } = counterSlice.actions // action-creator
 
 export default counterSlice.reducer
-console.log(counterSlice)
+// 测试获取action-type: increment
+console.log(counterSlice, '\b', increment())
+
+// createSlice生成了响应aciton的reducer,尝试更新state
+const newState = counterSlice.reducer({ value: 10 }, increment())
+console.log(newState) // {value: 11}
 
 // redux版本
 export const Counter = (state = 0, action) => {
