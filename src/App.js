460f34a2a1cee935269aad343dbfe05e00a4988d
wangshan <22212@qq.com>
2022-05-13 23:22:17 +0800

    feat: 路由跳转添加title

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 /*
- * @LastEditTime: 2022-05-13 23:14:38
+ * @LastEditTime: 2022-05-13 23:20:57
  * @Description:
  * @Date: 2022-03-15 23:30:51
  * @Author: wangshan
  * @LastEditors: wangshan
  */
-import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+
+import { Routes, Route, useLocation } from 'react-router-dom'
 
 import Layout from './layout/Layout'
 import routers from '@/router/index.js'
 
-import '@/style/global/App.scss'
+export default function App() {
+    const location = useLocation()
+    useEffect(() => {
+        document.title = location.pathname.split('/')[1]
+    }, [location])
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    {routers.map((v) => {
-                        return (
-                            <Route
-                                key={v.name}
-                                path={v.path}
-                                element={<v.component />}
-                            ></Route>
-                        )
-                    })}
-                </Route>
-            </Routes>
-        )
-    }
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {routers.map((v) => {
+                    return (
+                        <Route
+                            key={v.name}
+                            path={v.path}
+                            element={<v.component />}
+                        ></Route>
+                    )
+                })}
+            </Route>
+        </Routes>
+    )
 }
