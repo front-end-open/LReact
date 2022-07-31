d22481f8bb490ff5cbcb73d79ca949654ed4cc76
wangshan <22212@qq.com>
2022-08-01 01:15:36 +0800

    feat(Tab): Tab基础内容子组件

--- src/views/coponent/components/Tabr/TabrNav.jsx
+++ src/views/coponent/components/Tabr/TabrNav.jsx
@@ -1,7 +1,79 @@
 /*
- * @LastEditTime: 2022-08-01 00:50:25
+ * @LastEditTime: 2022-08-01 01:05:48
  * @Description: TabrNav
  * @Date: 2022-08-01 00:50:25
  * @Author: wangshan
  * @LastEditors: wangshan
  */
+import React, { Component } from 'react'
+
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+
+export default class TabNav extends Component {
+    static propTypes = {
+        classPrefix: PropTypes.string,
+        panels: PropTypes.node,
+        activeIndex: PropTypes.number,
+        onTabClick: PropTypes.func
+    }
+    getTabs() {
+        const { panels, classPrefix, activeIndex } = this.props
+
+        return React.Children.map(panels, (child) => {
+            if (!child) {
+                return
+            }
+
+            const order = parseInt(child.props.order, 10)
+
+            //
+            let classes = classnames({
+                [`${classPrefix}-tab`]: true,
+                [`${classPrefix}-active`]: activeIndex === order,
+                [`${classPrefix}-disabled`]: child.props.disabled
+            })
+
+            let events = {}
+            if (!child.props.disabled) {
+                events = {
+                    onClick: this.props.onTabClick.bind(this, order)
+                }
+            }
+            const ref = {}
+            if (activeIndex === order) {
+                ref.ref = 'activeTab'
+            }
+            return (
+                <li
+                    role={'tab'}
+                    aria-disabled={child.props.disabled ? 'true' : 'false'}
+                    aria-selected={activeIndex === order ? 'true' : 'false'}
+                    {...events}
+                    className={classes}
+                    key={order}
+                    {...ref}
+                >
+                    {child.props.tab}
+                </li>
+            )
+        })
+    }
+    render() {
+        const { classPrefix } = this.props
+
+        const rootClasses = classnames({
+            [`${classPrefix}-bar`]: true
+        })
+
+        const classes = classnames({
+            [`${classPrefix}-nav`]: true
+        })
+
+        return (
+            <div className={rootClasses} role="tablist">
+                <ul className={classes}>{this.getTabs()}</ul>
+            </div>
+        )
+    }
+}
