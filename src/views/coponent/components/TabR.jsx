db362b3e745844e68a7adf9deb8bd144bd600872
wangshan <22212@qq.com>
2022-08-01 00:37:30 +0800

    feat: 组件深入,prop/state

---
+++ src/views/coponent/components/TabR.jsx
@@ -0,0 +1,16 @@
+/*
+ * @LastEditTime: 2022-07-31 22:42:45
+ * @Description: React版tab实现
+ * @Date: 2022-07-31 22:40:33
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+import React, { Component } from 'react'
+
+class Tabs extends Component {
+    render() {
+        return <div className="ui-tabs"></div>
+    }
+}
+
+export default Tabs
