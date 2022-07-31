ccf58cc60d8c9e1183cb3cab5436595721ed9073
wangshan <22212@qq.com>
2022-08-01 00:45:45 +0800

    feat: react生命周期

--- src/views/coponent/components/Tabr/index.jsx
+++ src/views/coponent/components/Tabr/index.jsx
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-07-31 22:42:45
+ * @LastEditTime: 2022-08-01 00:44:40
  * @Description: React版tab实现
  * @Date: 2022-07-31 22:40:33
  * @Author: wangshan
@@ -7,10 +7,10 @@
  */
 import React, { Component } from 'react'
 
-class Tabs extends Component {
+class Tabr extends Component {
     render() {
         return <div className="ui-tabs"></div>
     }
 }
 
-export default Tabs
+export default Tabr
