a64ee2ad5badb55cd20e18584a9c7bd14ec76ca1
wangshan <22212@qq.com>
2022-08-06 22:43:23 +0800

    feat(event): 合成事件机制原理

--- src/views/EventSystem/index.js
+++ src/views/EventSystem/index.js
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-08-06 17:43:00
+ * @LastEditTime: 2022-08-06 22:28:22
  * @Description: 事件系统
  * @Date: 2022-08-06 16:43:43
  * @Author: wangshan
@@ -7,7 +7,8 @@
  */
 import { Component } from 'react'
 
-import Toggle from './components/Toggle'
+import BubbleOrCapture from './components/EventProcess'
+// import Toggle from './components/Toggle'
 
 export default class EventSystem extends Component {
     render() {
@@ -16,8 +17,12 @@ export default class EventSystem extends Component {
                 <h2 className="primary">事件系统</h2>
                 <p className="primary">开关组件</p>
 
+                <div>{/* <Toggle title="开关" /> */}</div>
+
                 <div>
-                    <Toggle title="开关" />
+                    <h2>事件执行阶段</h2>
+
+                    <BubbleOrCapture />
                 </div>
             </div>
         )
