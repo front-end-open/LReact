7d58394bed5fc7ab12654cb27e95f9b1865b9010
wangshan <22212@qq.com>
2022-08-14 00:43:18 +0800

    feat(axios-api): add api config

---
+++ src/service/pathPrefix.js
@@ -0,0 +1,13 @@
+/*
+ * @LastEditTime: 2022-08-14 00:40:46
+ * @Description:
+ * @Date: 2022-08-14 00:39:40
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+// 评论接口
+const comment = '/api/comment.json'
+
+export default {
+    comment
+}
