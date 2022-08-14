19850720692a7b76648fccd6561a42561afbb1c0
wangshan <22212@qq.com>
2022-08-15 02:02:26 +0800

    feat(inheritance-inversion): 高阶组件的输出结果劫持
    - 输出结果修改

--- src/views/coponent/components/Hoc/InheritanceInversion.js
+++ src/views/coponent/components/Hoc/InheritanceInversion.js
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/display-name */
-const { Component } = require('react')
+const { Component, cloneElement } = require('react')
 
 /*
- * @LastEditTime: 2022-08-15 01:29:13
+ * @LastEditTime: 2022-08-15 01:59:34
  * @Description: 高阶组件-反向继承-渲染劫持
  * @Date: 2022-08-15 01:17:14
  * @Author: wangshan
@@ -14,7 +15,20 @@ const Author = (wrappedComponent) => {
     return class extends wrappedComponent {
         render() {
             if (this.props.isLogin) {
-                return super.render()
+                const element = super.render()
+                console.log(element, 'home-insca')
+                // 必须是input类型才可以传递value
+                const props = Object.assign({}, element.props, {
+                    value: 'may the force be with you'
+                })
+                console.log(props, 'home-props')
+                const elementTree = cloneElement(
+                    element,
+                    props,
+                    element.props.children
+                )
+
+                return elementTree
             } else {
                 return (
                     <div>
@@ -26,12 +40,18 @@ const Author = (wrappedComponent) => {
         }
     }
 }
+function CusInput({ value }) {
+    // console.log('value', vlaue)
+    return <input type="text" value={value} />
+}
 
 class Home extends Component {
     render() {
+        console.log(this.props, 'Home')
         return (
             <div>
-                <h3 className="tip-content"> webcome back</h3>
+                <h3>welcome back</h3>
+                <CusInput value={this.props.value} />
             </div>
         )
     }
