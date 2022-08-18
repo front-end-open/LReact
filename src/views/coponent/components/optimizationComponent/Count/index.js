73ddcfbc0b4ad7ea00969ac581a5ac6286b4a721
wangshan <22212@qq.com>
2022-08-19 01:51:22 +0800

    feat(optimizationComponent): PureRender基类使用

---
+++ src/views/coponent/components/optimizationComponent/Count/index.js
@@ -0,0 +1,87 @@
+/* eslint-disable react/display-name */
+/*
+ * @LastEditTime: 2022-08-19 01:50:21
+ * @Description: PureRender使用
+ * @Date: 2022-08-19 01:13:24
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+// 对props和state做浅比较实现一个计数器
+// 浅比较可以使用官方的`React.PureCompoent`
+// 该基础类内部实现了对`props`和`state`的浅比较
+// 但是对于复杂的数据类型需要注意，这可能导致数据更新不及时
+import { Component, PureComponent } from 'react'
+
+import PropTypes from 'prop-types'
+
+class Count extends PureComponent {
+    static propTypes = {
+        count: PropTypes.number,
+        add: PropTypes.func,
+        curry: PropTypes.func
+    }
+    componentDidUpdate() {
+        console.log(this.props, 1111111111111)
+    }
+
+    render() {
+        const { count, curry, add } = this.props
+        return (
+            <div style={{ dispaly: 'flex' }}>
+                <button onClick={curry}>Curry</button>
+                <span style={{ margin: '0 12px' }}>{count}</span>
+                <button onClick={add}>Add</button>
+            </div>
+        )
+    }
+}
+
+const withCount = (WrappedComponent) => {
+    return class extends Component {
+        constructor(props) {
+            super(props)
+            this.state = {
+                count: 0
+            }
+            this.handleAdd = this.handleAdd.bind(this)
+            // this.handleCurry = this.handleCurry.bind(this)
+            // this.handleAdd.bind(this)
+        }
+
+        handleCurry() {
+            console.log(this)
+            // this.setState((preState) => {
+            //     return {
+            //         count: preState.count--
+            //     }
+            // })
+        }
+        handleAdd() {
+            console.log(this)
+            this.setState((preState) => {
+                let pcount = preState.count
+                return {
+                    count: 10
+                }
+            })
+            // let preState = this.state.count
+            // this.setState({
+            //     count: preState++
+            // })
+        }
+
+        render() {
+            return (
+                <WrappedComponent
+                    {...this.props}
+                    add={this.handleAdd}
+                    count={this.state.count}
+                    curry={this.handleCurry}
+                />
+            )
+        }
+    }
+    // return doneCount
+}
+
+export default withCount(Count)
--- src/views/coponent/components/optimizationComponent/index.js
+++ src/views/coponent/components/optimizationComponent/index.js
@@ -1,7 +1,18 @@
 /*
- * @LastEditTime: 2022-08-18 22:56:44
+ * @LastEditTime: 2022-08-19 01:27:15
  * @Description:
  * @Date: 2022-08-18 22:56:43
  * @Author: wangshan
  * @LastEditors: wangshan
  */
+import Count from './Count'
+
+export function Optimization() {
+    return (
+        <div className="tip-content">
+            <h3 className="primary">PureRender使用</h3>
+
+            <Count />
+        </div>
+    )
+}
