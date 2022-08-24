4b983e1ee5f15455efad34d24a01a211df5a4559
wangshan <wangshan@nbtide.net>
2022-08-25 01:24:01 +0800

    feat(examine-fade): 过渡动效

---
+++ src/views/Animation/base/Fade.js
@@ -0,0 +1,44 @@
+/*
+ * @LastEditTime: 2022-08-25 00:53:41
+ * @Description:
+ * @Date: 2022-08-25 00:47:56
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+import PropTypes from 'prop-types'
+import { Transition } from 'react-transition-group'
+
+const duration = 300
+
+const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0
+}
+
+const transitionStyles = {
+    entering: { opacity: 1 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 }
+}
+
+const Fade = ({ in: inProp }) => (
+    <Transition in={inProp} timeout={duration}>
+        {(state) => (
+            <div
+                style={{
+                    ...defaultStyle,
+                    ...transitionStyles[state]
+                }}
+            >
+                I'm a fade Transition!--{state}
+            </div>
+        )}
+    </Transition>
+)
+
+Fade.propTypes = {
+    in: PropTypes.bool
+}
+
+export default Fade
--- src/views/Animation/index.js
+++ src/views/Animation/index.js
@@ -1,15 +1,29 @@
 /*
- * @LastEditTime: 2022-08-24 23:32:47
+ * @LastEditTime: 2022-08-25 00:58:05
  * @Description: 动画
  * @Date: 2022-08-24 23:32:46
  * @Author: wangshan
  * @LastEditors: wangshan
  */
+
+import { useState } from 'react'
+
+import Fade from './base/Fade'
+
 function Animation() {
+    const [state, setState] = useState(false)
+
     return (
-        <div>
+        <div className="tip-content">
             <h3 className="primary">动画</h3>
+
+            <Fade in={state} />
+
+            <button onClick={() => (!state ? setState(true) : setState(false))}>
+                start
+            </button>
         </div>
     )
 }
+
 export default Animation
